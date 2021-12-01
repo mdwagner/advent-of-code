efbbd14b105b481a8db0a774e08f0a0745a8bb3d
Michael Wagner <mdwranger@gmail.com>
2021-12-01 13:56:01 -0500

    fix: remove bang

--- 2021/src/main.zig
+++ 2021/src/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     std.log.info("TODO", .{});
 }
 
-fn day1(depths: []const u32) !u32 {
+fn day1(depths: []const u32) u32 {
     var count: u32 = 0;
     for (depths) |depth, index| {
         if (index == 0) {
@@ -20,6 +20,6 @@ fn day1(depths: []const u32) !u32 {
 
 test "Day 1" {
     const measurements = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
-    const num_of_measurements = try day1(measurements[0..]);
+    const num_of_measurements = day1(measurements[0..]);
     try expect(num_of_measurements == 7);
 }
