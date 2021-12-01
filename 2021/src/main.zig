3794803a0a56a2ea80f02767916f83618763a06e
Michael Wagner <mdwranger@gmail.com>
2021-12-01 09:52:22 -0500

    feat(wip): day 1

--- 2021/src/main.zig
+++ 2021/src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub fn main() !void {
+    std.log.info("TODO", .{});
+}
+
+fn day1() !void {}
+
+test "Day 1" {
+    const items = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+    _ = try day1();
+    try expect(false);
 }
