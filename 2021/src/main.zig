cf6f27df346b25980a847d444554e6ccc135c84c
Michael Wagner <mdwranger@gmail.com>
2021-12-01 13:53:37 -0500

    fix: day 1 works!

--- 2021/src/main.zig
+++ 2021/src/main.zig
@@ -5,10 +5,21 @@ pub fn main() !void {
     std.log.info("TODO", .{});
 }
 
-fn day1() !void {}
+fn day1(depths: []const u32) !u32 {
+    var count: u32 = 0;
+    for (depths) |depth, index| {
+        if (index == 0) {
+            continue;
+        }
+        if (depth > depths[index - 1]) {
+            count += 1;
+        }
+    }
+    return count;
+}
 
 test "Day 1" {
-    const items = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
-    _ = try day1();
-    try expect(false);
+    const measurements = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+    const num_of_measurements = try day1(measurements[0..]);
+    try expect(num_of_measurements == 7);
 }
