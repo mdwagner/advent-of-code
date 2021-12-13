9281524e704cffd766284e7cbcee3f18e196a637
Michael Wagner <mdwranger@gmail.com>
2021-12-12 20:11:49 -0500

    feat: day 7 completed

--- 2021/crystal/spec/day7_spec.cr
+++ 2021/crystal/spec/day7_spec.cr
@@ -3,19 +3,20 @@ require "./spec_helper"
 Spectator.describe AdventOfCode::Day7 do
   subject {
 <<-TEXT
+16,1,2,0,4,2,7,1,2,14
 TEXT
   }
   let(input) { AdventOfCode::Helpers.string_lines_to_a(subject) }
 
   describe "#a" do
     it "works" do
-      expect(described_class.new.a(input)).to eq(0)
+      expect(described_class.new.a(input)).to eq(37)
     end
   end
 
   describe "#b" do
     it "works" do
-      expect(described_class.new.b(input)).to eq(0)
+      expect(described_class.new.b(input)).to eq(168)
     end
   end
 end
--- 2021/crystal/src/days/day7.cr
+++ 2021/crystal/src/days/day7.cr
@@ -1,7 +1,31 @@
 class AdventOfCode::Day7
-  def a(input : Array(String))
+  def a(input : Array(String), part_2 = false)
+    crabs = input.first.split(",").map(&.to_u32)
+
+    min = crabs.min
+    max = crabs.max
+
+    cheapest_fuel = UInt32::MAX
+
+    (min..max).each do |pos|
+      fuel_outcome = crabs.sum do |c|
+        n = (c.to_i - pos.to_i).abs
+        if part_2
+          0.5 * n * (n + 1)
+        else
+          n
+        end
+      end.to_i
+
+      if fuel_outcome < cheapest_fuel
+        cheapest_fuel = fuel_outcome
+      end
+    end
+
+    cheapest_fuel
   end
 
   def b(input : Array(String))
+    a(input, true)
   end
 end
