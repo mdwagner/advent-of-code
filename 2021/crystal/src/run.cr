9d8aba39d7c272559455c95fa1f8e9e99343d169
Michael Wagner <mdwranger@gmail.com>
2022-01-02 21:01:32 -0500

    chore: formatting

--- 2021/crystal/src/run.cr
+++ 2021/crystal/src/run.cr
@@ -1,17 +1,18 @@
 require "./advent_of_code"
 
 {% begin %}
-{% day_count = system("ls -1 #{__DIR__}/../../_inputs/ | wc -l").to_i %}
-{% for day in 1..day_count %}
-{% contents = read_file("#{__DIR__}/../../_inputs/day#{day}.txt") %}
+  {% day_count = system("ls -1 #{__DIR__}/../../_inputs/ | wc -l").to_i %}
+  {% for day in 1..day_count %}
+  {% contents = read_file("#{__DIR__}/../../_inputs/day#{day}.txt") %}
+    day{{day.id}}_input = AdventOfCode::Helpers.string_lines_to_a({{contents.id.stringify}})
+    day{{day.id}}_instance = AdventOfCode::Day{{day.id}}.new
+    day{{day.id}}_a_result = day{{day.id}}_instance.a(day{{day.id}}_input)
+    day{{day.id}}_b_result = day{{day.id}}_instance.b(day{{day.id}}_input)
+    puts "Day {{day.id}}a: #{day{{day.id}}_a_result}"
+    puts "Day {{day.id}}b: #{day{{day.id}}_b_result}"
+  {% unless day == day_count %}
+    puts
+  {% end %}
 
-day{{day.id}}_input = AdventOfCode::Helpers.string_lines_to_a({{contents.id.stringify}})
-day{{day.id}}_instance = AdventOfCode::Day{{day.id}}.new
-day{{day.id}}_a_result = day{{day.id}}_instance.a(day{{day.id}}_input)
-day{{day.id}}_b_result = day{{day.id}}_instance.b(day{{day.id}}_input)
-puts "Day {{day.id}}a: #{day{{day.id}}_a_result}"
-puts "Day {{day.id}}b: #{day{{day.id}}_b_result}"
-puts
-
-{% end %}
+  {% end %}
 {% end %}
